eff15e4480aac95b4b9cbdb1a61e4659533578f0
Alex D <68231126+andestef@users.noreply.github.com>
2026-05-26 12:50:54 -0400

    Real Quick Push

--- src/Conwayplusplus.java
+++ src/Conwayplusplus.java
@@ -2,7 +2,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -10,23 +10,22 @@ public class Conwayplusplus extends Application {
    public static void main(String[] args) {
         launch(args);
     }
-    
-    @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Conway++");
+        Label conplusplus = new Label("Conway++");
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
  
-            @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
             }
         });
-        
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
-} 
+}
